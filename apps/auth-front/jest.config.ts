5dea680a883cd30bc5957f6d881ee4fc814a05de
Palahin Vladyslav <palahinvlad@gmail.com>
2024-06-20 17:37:31 +0300

    fix: renamed app

--- apps/auth-front/jest.config.ts
+++ apps/auth-front/jest.config.ts
@@ -1,9 +1,9 @@
 /* eslint-disable */
 export default {
-  displayName: 'auth-app',
+  displayName: 'auth-front',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  coverageDirectory: '../../coverage/apps/auth-app',
+  coverageDirectory: '../../coverage/apps/auth-front',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': [
       'jest-preset-angular',
